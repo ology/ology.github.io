dc6812c8f0bcee36d091dca70ef52cda33042eb0
Gene Boggs <gene@ology.net>
2020-12-15 10:07:26 -0800

    Deploy

--- theme/site/ology.js
+++ theme/site/ology.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-    console.log("ready!");
+    console.log("Ready!");
+    $("#query").on('keypress', function(e) {
+        if(e.which == 13) {
+            var v = $("#query").val();
+            window.location.href = "https://www.google.com/search?q=site%3Aology.github.io+" + v;
+        }
+    });
 });
